7da6f2e5796c90249ecb652b38123a13860271c6
akshaykumar998 <akshaynadagouda998@gmail.com>
2023-02-13 15:43:59 +0530

    second backup

--- src/main/java/com/project/operationcrud/OperationCrudApplication.java
+++ src/main/java/com/project/operationcrud/OperationCrudApplication.java
@@ -1,4 +1,4 @@
-package com.project.operationcrud.exceptions;
+package com.project.operationcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
